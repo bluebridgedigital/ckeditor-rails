7e610761f8cfa4ff2f6902027dca1dbd1c09b693
Eddie Hourigan <ehourigan@developertown.com>
2014-02-26 16:39:15 -0500

    trying something different 7

--- vendor/assets/javascripts/ckeditor/plugins/blankify/plugin.js
+++ vendor/assets/javascripts/ckeditor/plugins/blankify/plugin.js
@@ -7,11 +7,23 @@ CKEDITOR.plugins.add( 'blankify',
       {
         exec : function( editor )
         {   
-          editor.applyStyle(new CKEDITOR.style({
-            Element : 'span', 
-            Attributes : { 'class' : 'blankify' },
-            Styles: {'color' : '#A8A8A8'}
+          var style = new CKEDITOR.style({
+            element : 'span', 
+            attributes : { 'class' : 'blankify' },
+            styles: {'color' : '#A8A8A8'}
           }));
+          
+          style.type = CKEDITOR.STYLE_INLINE;
+          style.apply( editor.document );
+
+          var selection = editor.document.getSelection();
+          var element1 = new CKEDITOR.dom.element( 'element1', editor.document );
+          if (selection.getStartElement().getName() == 'span')
+          {
+             element1.insertBefore( selection.getStartElement() );
+             element1.append(selection.getStartElement());
+          }
+
           //editor.applyStyle(new CKEDITOR.style({Element : 'p', Attributes : { class : 'Myclass' }, Styles : { color : '#ff0000'} )); 
           //var selection = editor.getSelection().getSelectedText();
           //var newElement = new CKEDITOR.dom.element("em"); 
