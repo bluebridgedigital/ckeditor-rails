70a2244ceab25480837585bd85a1ffa5e918bad4
Eddie Hourigan <ehourigan@developertown.com>
2014-02-26 14:23:30 -0500

    blankify dev test

---
+++ vendor/assets/javascripts/ckeditor/plugins/blankify/plugin.js
@@ -0,0 +1,23 @@
+CKEDITOR.plugins.add( 'blankify',
+{
+  init: function( editor )
+  {
+    //Plugin logic goes here.
+    editor.addCommand( 'insertBlankify',
+      {
+        exec : function( editor )
+        {    
+          var selection = editor.getSelection().getSelectedText();
+          var newElement = new CKEDITOR.dom.element("blankify"); 
+          newElement.setText(selected_text);                         
+          editor.insertElement(newElement);   
+        }
+      });
+    editor.ui.addButton( 'blankify',
+    {
+      label: 'blankify text',
+      command: 'insertBlankify',
+      icon: this.path + 'images/blankify.png'
+    } );
+  }
+} );
