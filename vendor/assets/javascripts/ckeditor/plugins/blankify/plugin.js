cd8dcc637b34ff3ae232abc5c64f3bdf4d219ad6
Eddie Hourigan <ehourigan@developertown.com>
2014-03-11 15:31:40 -0400

    tryin unique id 3

--- vendor/assets/javascripts/ckeditor/plugins/blankify/plugin.js
+++ vendor/assets/javascripts/ckeditor/plugins/blankify/plugin.js
@@ -3,7 +3,30 @@ CKEDITOR.plugins.add( 'blankify',
   init: function( editor )
   {
 
-    var style = new CKEDITOR.style({
+    // var style = new CKEDITOR.style({
+    //   element : 'span', 
+    //   attributes : { 'class' : 'blankify',
+    //                   'id': Math.round(new Date().getTime() + (Math.random() * 100)) 
+    //                 },
+    //   styles: {'background-color' : '#A8A8A8'}
+    // });
+
+    // editor.attachStyleStateChange( style, function( state ) {
+    //   editor.getCommand( 'insertBlankify' ).setState( state );
+    // } );
+
+    // editor.addCommand( 'insertBlankify', new CKEDITOR.styleCommand(style,
+    //   {
+    //   }) );
+    editor.ui.addButton( 'blankify',
+    {
+      label: 'blankify text',
+      command: 'insertBlankify',
+      icon: this.path + 'images/blankify.png'
+    } );
+  },
+  exec: function(editor){
+        var style = new CKEDITOR.style({
       element : 'span', 
       attributes : { 'class' : 'blankify',
                       'id': Math.round(new Date().getTime() + (Math.random() * 100)) 
@@ -17,16 +40,6 @@ CKEDITOR.plugins.add( 'blankify',
 
     editor.addCommand( 'insertBlankify', new CKEDITOR.styleCommand(style,
       {
-        exec : function( editor )
-          {  
-            alert('hello');
-          }
       }) );
-    editor.ui.addButton( 'blankify',
-    {
-      label: 'blankify text',
-      command: 'insertBlankify',
-      icon: this.path + 'images/blankify.png'
-    } );
   }
 } );
