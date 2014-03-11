90afb7ba6be1cdd8ce266d8ca8dcba93ace5c4af
Eddie Hourigan <ehourigan@developertown.com>
2014-03-11 15:00:34 -0400

    trying something new 1

--- vendor/assets/javascripts/ckeditor/plugins/blankify/plugin.js
+++ vendor/assets/javascripts/ckeditor/plugins/blankify/plugin.js
@@ -2,34 +2,44 @@ CKEDITOR.plugins.add( 'blankify',
 {
   init: function( editor )
   {
+
+        var style = new CKEDITOR.style({
+          element : 'span', 
+          attributes : { 'class' : 'blankify' },
+          styles: {'background-color' : '#A8A8A8'}
+        });
+ 
+        editor.attachStyleStateChange( style, function( state ) {
+          editor.getCommand( 'insertBlankify' ).setState( state );
+        } );
     //Plugin logic goes here.
-    editor.addCommand( 'insertBlankify',
+    editor.addCommand( 'insertBlankify', new CKEDITOR.styleCommand(style,
       {
-        exec : function( editor )
-        {   
-          var style = new CKEDITOR.style({
-            element : 'span', 
-            attributes : { 'class' : 'blankify' },
-            styles: {'background-color' : '#A8A8A8'}
-          });
+        // exec : function( editor )
+        // {   
+        //   var style = new CKEDITOR.style({
+        //     element : 'span', 
+        //     attributes : { 'class' : 'blankify' },
+        //     styles: {'background-color' : '#A8A8A8'}
+        //   });
           
-          style.type = CKEDITOR.STYLE_INLINE;
-          var selection = editor.document.getSelection();
-          var ele = selection.getStartElement();
-          if(style.checkElementMatch(ele,false)){
-            var range = selection.getRanges()[0];
-            style.removeFromRange(range);
-            style.remove(editor.document);
-            return;
+        //   style.type = CKEDITOR.STYLE_INLINE;
+        //   var selection = editor.document.getSelection();
+        //   var ele = selection.getStartElement();
+        //   if(style.checkElementMatch(ele,false)){
+        //     var range = selection.getRanges()[0];
+        //     style.removeFromRange(range);
+        //     style.remove(editor.document);
+        //     return;
 
-          }
+        //   }
 
-          var element1 = new CKEDITOR.dom.element( 'span', editor.document );
-          element1.setText( selection.getSelectedText() );
-          editor.insertElement(element1);
-          style.applyToObject(element1);  
-        }
-      });
+        //   var element1 = new CKEDITOR.dom.element( 'span', editor.document );
+        //   element1.setText( selection.getSelectedText() );
+        //   editor.insertElement(element1);
+        //   style.applyToObject(element1);  
+        // }
+      }) );
     editor.ui.addButton( 'blankify',
     {
       label: 'blankify text',
