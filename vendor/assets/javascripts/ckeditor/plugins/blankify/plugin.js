7696c9d1ca59dca74adb93ac5239d3694de62679
Eddie Hourigan <ehourigan@developertown.com>
2014-03-11 16:15:14 -0400

    tryin unique id 7

--- vendor/assets/javascripts/ckeditor/plugins/blankify/plugin.js
+++ vendor/assets/javascripts/ckeditor/plugins/blankify/plugin.js
@@ -3,32 +3,37 @@ CKEDITOR.plugins.add( 'blankify',
   init: function( editor )
   {
 
-    var style = new CKEDITOR.style({
-      element : 'span', 
-      attributes : { 'class' : 'blankify',
-                      'id': Math.round(new Date().getTime() + (Math.random() * 100)) 
-                    },
-      styles: {'background-color' : '#A8A8A8'}
-    });
+    // var style = new CKEDITOR.style({
+    //   element : 'span', 
+    //   attributes : { 'class' : 'blankify',
+    //                   'id': Math.round(new Date().getTime() + (Math.random() * 100)) 
+    //                 },
+    //   styles: {'background-color' : '#A8A8A8'}
+    // });
 
-    editor.attachStyleStateChange( style, function( state ) {
-      editor.getCommand( 'insertBlankify' ).setState( state );
+    // editor.attachStyleStateChange( style, function( state ) {
+    //   editor.getCommand( 'insertBlankify' ).setState( state );
+    // } );
+
+    // editor.addCommand( 'insertBlankify', new CKEDITOR.styleCommand(style,
+    //   {
+        
+    //   }) );
+    editor.addCommand( 'insertBlankify', {
+        exec: function( editor ) {
+          var style = new CKEDITOR.style({
+          element : 'span', 
+          attributes : { 'class' : 'blankify',
+                          'id': Math.round(new Date().getTime() + (Math.random() * 100)) 
+                        },
+          styles: {'background-color' : '#A8A8A8'}
+        });
+
+        editor.attachStyleStateChange( style, function( state ) {
+          editor.getCommand( 'insertBlankify' ).setState( state );
+        } );
+        }
     } );
-    alert('hi');
-    var cf = [
-      'b',
-       'i',
-      ['span' , function(el){
-        alert('hi');
-        el.styles['id'] = Math.round(new Date().getTime() + (Math.random() * 100));
-        return el.styles; 
-      }]
-    ];
-
-    editor.addCommand( 'insertBlankify', new CKEDITOR.styleCommand(style,
-      {
-        contentForms: cf
-      }) );
     editor.ui.addButton( 'blankify',
     {
       label: 'blankify text',
